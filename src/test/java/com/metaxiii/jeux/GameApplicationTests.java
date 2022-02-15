f4cece3d93377549c287fd38060fc282c1a4fc53
meta <lehchibi.gael@pm.me>
2022-02-15 09:19:30 +0100

    make test passed

--- src/test/java/com/metaxiii/jeux/GameApplicationTests.java
+++ src/test/java/com/metaxiii/jeux/GameApplicationTests.java
@@ -2,8 +2,10 @@ package com.metaxiii.jeux;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest()
+@ActiveProfiles("test")
 class GameApplicationTests {
 
     @Test
